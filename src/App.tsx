49a60497ddf36a7a2209bbaca3b1c9fa472a08e8
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-12-06 17:06:08 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className='text-center w-full'>
+      <h1 className='text-3xl font-bold underline'>Underlined Bold 3xl Text</h1>
+    </div>
+  );
+}
