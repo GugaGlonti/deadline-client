4388f4a55d9ce8428d20729bcddd04579ac62bb9
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-12-06 18:52:25 +0100

    Add custom colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pink-dark': '#FF5C8D',
+        pink: '#FF90BC',
+        'pink-light': '#FFC2E3',
+
+        'yellow-dark': '#FFC46E',
+        yellow: '#FFD56E',
+        'yellow-light': '#FFE9B5',
+
+        'green-dark': '#7BEF8C',
+        green: '#B9F18C',
+        'green-light': '#D7FFC2',
+
+        'blue-dark': '#5C90FF',
+        blue: '#90BCFF',
+        'blue-light': '#C2E3FF',
+
+        'purple-dark': '#B28CFF',
+        purple: '#D1B2FF',
+        'purple-light': '#EFD7FF',
+      },
+    },
   },
   plugins: [],
 };
