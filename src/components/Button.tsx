8fa0502b46bc2b4cd299821395c495dc1c897472
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-12-06 18:52:33 +0100

    Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+  color?: 'pink' | 'yellow' | 'green' | 'blue' | 'purple';
+  rounded?: boolean;
+  className?: string;
+}
+
+export default function Button({ text, onClick, color = 'pink', className = '', ...props }: ButtonProps) {
+  return (
+    <button
+      className={`
+      py-2 px-4 border-b-4 rounded-md
+      ${color === 'pink' && 'bg-pink hover:bg-pink-dark border-pink-dark'}
+      ${color === 'yellow' && 'bg-yellow hover:bg-yellow-dark border-yellow-dark'}
+      ${color === 'blue' && 'bg-blue hover:bg-blue-dark border-blue-dark'}
+      ${color === 'green' && 'bg-green hover:bg-green-dark border-green-dark'}
+      ${color === 'purple' && 'bg-purple hover:bg-purple-dark border-purple-dark'}
+      ${className}
+      `}
+      onClick={onClick}
+      {...props}>
+      {text}
+    </button>
+  );
+}
