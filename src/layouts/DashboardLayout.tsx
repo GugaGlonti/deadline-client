31e276dead8b8dcac740ec82d6f786ea73662e8a
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-12-07 02:07:48 +0100

    Add navigation link and icon to DashboardLayout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router';
+import { NavLink } from 'react-router-dom';
 
 export default function DashboardLayout() {
   return (
     <div className='bg-white'>
       <main className='flex'>
         <aside className='flex flex-col items-center justify-around w-64 h-screen p-4 bg-gray-100'>
-          <h1>aside stuff</h1>
-          <h1>aside stuff</h1>
-          <h1>aside stuff</h1>
-          <h1>aside stuff</h1>
+          <NavLink to=''>
+            <h1 className='text-2xl font-bold'>OVERVEIEW</h1>
+          </NavLink>
         </aside>
         <Outlet />
       </main>
--- src/modules/dashboard/Dashboard.tsx
+++ src/modules/dashboard/Dashboard.tsx
@@ -1,11 +1,21 @@
+import { IoAdd } from 'react-icons/io5';
+
 export default function Dashboard() {
   return (
-    <>
+    <div className='w-full'>
       <div className='flex flex-col'>
         <h1>test</h1>
         <h1>test</h1>
         <h1>test</h1>
+
+        <IoAdd
+          className={`
+          h-12 w-12
+          text-yellow hover:text-yellow-light bg-yellow-dark
+          rounded-full
+          `}
+        />
       </div>
-    </>
+    </div>
   );
 }
