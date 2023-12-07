a7cb4ee781fa96daaeb4a33e2fe6869cbbf9657b
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-12-07 02:00:23 +0100

    Add icons to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,5 @@
+import { SlLogin, SlLogout } from 'react-icons/sl';
+
 interface ButtonProps {
   text: string;
   onClick: () => unknown;
@@ -9,18 +11,18 @@ interface ButtonProps {
 export default function Button({ text, onClick, color = 'pink', className = '', ...props }: ButtonProps) {
   return (
     <button
-      className={`
-      py-2 px-4 border-b-4 rounded-md
+      className={`py-2 px-4 border-b-4 rounded-md align-middle
       ${color === 'pink' && 'bg-pink hover:bg-pink-dark border-pink-dark'}
       ${color === 'yellow' && 'bg-yellow hover:bg-yellow-dark border-yellow-dark'}
       ${color === 'blue' && 'bg-blue hover:bg-blue-dark border-blue-dark'}
       ${color === 'green' && 'bg-green hover:bg-green-dark border-green-dark'}
       ${color === 'purple' && 'bg-purple hover:bg-purple-dark border-purple-dark'}
-      ${className}
-      `}
+      ${className}`}
       onClick={onClick}
       {...props}>
       {text}
+      {text === 'Logout' && <SlLogout className='inline-block ml-2' />}
+      {text === 'Login' && <SlLogin className='inline-block ml-2' />}
     </button>
   );
 }
