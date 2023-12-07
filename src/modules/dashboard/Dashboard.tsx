394baa2bfd82da87a9e79ad703c4214a986b8f35
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-12-07 01:22:00 +0100

    Add Dashboard component

---
+++ src/modules/dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+export default function Dashboard() {
+  return (
+    <>
+      <div className='flex flex-col'>
+        <h1>test</h1>
+        <h1>test</h1>
+        <h1>test</h1>
+      </div>
+    </>
+  );
+}
