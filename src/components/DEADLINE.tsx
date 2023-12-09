a889aa12e5100dfefb4e35ef2372a4835a2ce658
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-12-09 12:13:17 +0100

    Add clickHandler to navigate to home page

--- src/components/DEADLINE.tsx
+++ src/components/DEADLINE.tsx
@@ -1,10 +1,19 @@
+import { useNavigate } from 'react-router';
+
 interface DEADLINEProps {
   className?: string;
 }
 
 export default function DEADLINE({ className, ...porps }: DEADLINEProps) {
+  const navigate = useNavigate();
+
+  function clickHandler() {
+    navigate('/');
+  }
+
   return (
     <h1
+      onClick={clickHandler}
       className={`text-3xl tracking-title  pl-4 ${className}`}
       {...porps}>
       DEADLINE
