80d41257d2a4d41807e9476955de29eda1c05437
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-12-11 19:26:04 +0100

    Refactor useQuery to useQueryParam

--- src/hooks/useQuery.tsx
+++ src/hooks/useQuery.tsx
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 import { useLocation } from 'react-router';
 
-export default function useQuery(query: string) {
+export default function useQueryParam(query: string) {
   const { search } = useLocation();
 
   return useMemo(() => new URLSearchParams(search), [search]).get(query);
