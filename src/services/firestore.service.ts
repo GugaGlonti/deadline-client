a6d33b7a84cd0f4b153be65b9be7c739f36112eb
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-12-11 20:07:19 +0100

    cleanup

--- src/services/firestore.service.ts
+++ src/services/firestore.service.ts
@@ -3,12 +3,9 @@ import { Deadline } from '../types/Deadline';
 import { auth, db } from '../config/firebase.config';
 
 export class FirestoreService {
-  static deadlinesCol = collection(db, 'deadlines');
-
   public static async getDeadlines() {
     try {
       const data = await getDocs(query(collection(db, 'deadlines'), where('userId', '==', auth.currentUser?.uid || '')));
-      console.log('ADANE');
       return data.docs.map(doc => doc.data() as Deadline);
     } catch (error) {
       console.error(error);
