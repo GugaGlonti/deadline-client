4006fc8796a1a06c3320eb1252322a9c56bb5c73
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-12-25 16:22:12 +0400

    Add user authentication token handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import useAuthBasedRoutes from './hooks/useAuthBasedRoutes';
+import { auth } from './config/firebase.config';
 
 const queryClient = new QueryClient();
 
 export default function App() {
   const Routes = useAuthBasedRoutes();
+
+  auth.onAuthStateChanged(async user => {
+    if (user) localStorage.setItem('token', await user.getIdToken());
+    else localStorage.removeItem('token');
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
